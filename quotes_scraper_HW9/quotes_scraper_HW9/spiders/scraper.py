da065dbbaa5f352ac143362381e824a619b0563e
Dmytro-Ostrenko <142011911+Dmytro-Ostrenko@users.noreply.github.com>
2024-03-04 20:45:50 +0200

    Add files via upload

---
+++ quotes_scraper_HW9/quotes_scraper_HW9/spiders/scraper.py
@@ -0,0 +1,31 @@
+import scrapy
+from .items import QuoteItem, AuthorItem
+
+class ScraperSpider(scrapy.Spider):
+    name = 'scraper'
+    start_urls = ['http://quotes.toscrape.com']
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            item = QuoteItem()
+            item['quote'] = quote.css('span.text::text').get()
+            item['author'] = quote.css('span small.author::text').get()
+            item['tags'] = quote.css('div.tags a.tag::text').getall()
+            yield item
+
+            # Extract author information
+            author_link = quote.css('span a::attr(href)').get()
+            yield response.follow(author_link, self.parse_author)
+
+        # Follow pagination link
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
+
+    def parse_author(self, response):
+        item = AuthorItem()
+        item['fullname'] = response.css('h3.author-title::text').get()
+        item['born_date'] = response.css('span.author-born-date::text').get()
+        item['born_location'] = response.css('span.author-born-location::text').get()
+        item['description'] = response.css('div.author-description::text').get()
+        yield item
